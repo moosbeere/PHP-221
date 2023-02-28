0fa2651bf4b354137ab8d5f5a09c958cb3e6bcc1
moosbeere <moosbeere_O@mail.ru>
2023-02-28 12:11:14 +0300

    Lab3

---
+++ 221-323/3Lab/index.php
@@ -0,0 +1,102 @@
+<?php
+// Объявление переменной
+    // $a = 2;
+    // define("pi", 3.14);
+    // $R=20;
+    // $L = 2 * $R * pi;
+    // echo "<h2>$L</h2>$L</br>";
+    // echo '<h2>'.$L.'</h2>';
+
+    // типы данных
+    // $f = 2;
+    // $s = $f.'hello';
+    // $b = false;
+    // // $c = (float)$f;
+    // // printf ('%.3f', $c);
+    // echo '<br>';
+    // // echo $f + $s;
+    // echo '<br>';
+    // echo "\$b=: $b";
+    // echo '<br>';
+    // echo '$b='.$b.' type: '.gettype($b);
+
+    //округление
+
+    // $a = 3.14758587578;
+    // echo "\$a = $a</br>";
+    // echo 'floor: '.floor($a);
+    // echo '<br>';
+    // echo 'round '.round($a).'<br>';
+    // echo 'ceil '.ceil($a);
+
+    //форматирование переменной для печати
+
+    // $a = 3.14;
+    // printf('%.8f', $a);
+    // echo '<br>';
+    // $d=5;
+    // printf("%'.6d", $d);
+
+    //операции над скалярами
+    // $a = true;
+    // $b = false;
+    // echo "\$a = $a<br>";
+    // echo "\$b = $b<BR>";
+    // echo '+ '.$a + $b.'<br>';
+    // echo '- '.$a - $b.'<br>';
+    // echo '* '.$a * $b.'<br>';
+    // // echo '/ '.$a / $b.'<br>';
+    // // echo '% '.$a % $b.'<br>';
+    // echo '2<sup>10</sup>='.pow(2, 10).'<br>';
+
+    // // var_dump($a===$b);
+    // var_dump($a==$b);
+    // var_dump($a>=$b);
+    // echo '<br>';
+    // var_dump(!$a);
+
+    // $c = 6;
+    // echo $c++.'<br>';
+    // echo ++$c.'<br>';
+    // // echo $c+=2;
+    // echo $c--.'<br>';
+    // echo --$c.'<br>';
+
+    //условные операторы
+    $a = 0;
+    $b = 5;
+    // if ($a < $b){
+    //     $c = $b - $a;
+    //     echo $c;
+    // }
+    // // if ($a < $b) echo $b-$a;
+    // elseif ($a > $b) echo $a - $b;
+    // else echo 'ok';
+
+    // if($a) echo $a;
+
+    // switch($a + $b){
+    //     case 10: 
+    //         echo $a + $b;
+    //         break;
+    //     case 0: 
+    //         echo 'ok';
+    //         break;
+    //     default: echo 'default';
+    // }
+
+    // while ($a!=0){
+    //     echo --$a;
+    // }
+
+    // do{
+    //     echo --$a;
+    // }while ($a > 0);
+
+    for ($i = 7; $i != 10; $i++) 
+    echo $i.'<br>';
+
+
+        
+
+?>
