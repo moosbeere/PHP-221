f8adf36fd91dcb0b2c88304536ea920c409b7b56
moosbeere <moosbeere_O@mail.ru>
2023-02-28 15:21:39 +0300

    Lab3

---
+++ 221-322/Lab3/index.php
@@ -0,0 +1,45 @@
+<?php
+    $a = 0;
+    $b = 3;
+    // echo $a + $b;
+
+    // echo $a+=2;
+
+    // if ($a < $b) echo '$a < $b';
+    // elseif ($a > $b) {
+    //     $c = $a + $b;
+    //     echo $c;
+    // }
+    // else echo 'ok';
+
+    // $min=($a < $b) ? $a:$b;
+    // echo $min;
+
+    // switch($a + $b){
+    //     case 10: 
+    //         echo '10';
+    //         break;
+    //     case 20: 
+    //         echo '20';
+    //         break;
+    //     case 2: 
+    //         echo 'ok';
+    //         break;
+    //     default: 
+    //         echo 'default';
+    // }
+
+    // while ($a != 15) {
+    //     print(++$a);
+    //     echo '<br>';
+    // }
+
+    // do{
+    //     print(--$a);
+    //     echo '<br>';     
+    // }while($a > 0);
+
+    for ($i = 3; $i <= 10; $i++){
+        if ($i == 5) break;
+        echo "$i<br>";
+    }
---
+++ 221-322/Lab3/index2.php
@@ -0,0 +1,64 @@
+<?php
+    $a = 0;
+    define("pi", 3.14);
+    $R = 2;
+    $L = 2*pi*$R;
+    // echo $L;
+
+    $f = 2.2;
+
+    // echo gettype($f);
+
+    $s = '2hello';
+    // echo gettype($s);
+
+    $b = (int)$s;
+    // echo gettype($b);
+    // echo $b;
+
+    $bool = true;
+    // var_dump($bool);
+
+    // var_dump((bool)$a);
+
+    $floor = 3.143;
+    // echo "\$floor = $floor<br>";
+    // echo 'floor: '.floor($floor).'<br>';
+    // echo 'round: '.round($floor).'<br>';
+    // echo 'ceil: '.ceil($floor).'<br>';
+
+    $x = 2.3;
+    // printf("%'.05d\n", $x);
+
+    $c = 4;
+    $v = 0;
+    // echo '$c + $v = '.$c+$v.'<br>';
+    // echo '$c - $v = '.$c-$v.'<br>';
+    // echo '$c * $v = '.$c*$v.'<br>';
+    // echo '$c / $v = '.$c/$v.'<br>';
+    // echo '$c % $v = '.$c%$v.'<br>';
+    
+    // echo  '2<sup>10</sup>='.pow(2, 10);
+
+    // var_dump($c === $v);
+
+    $e = '2';
+    $h = '2a';
+
+    // echo $e + $h;
+    // echo '<br>';
+    // var_dump($h > $e);
+
+    // var_dump(!$c);
+
+    // $r = 6;
+    // echo $r++;
+    // echo '<br>';
+    // echo ++$r;
+
+    // echo $r--;
+    // echo '<br>';
+    // echo --$r; 
+
+
+?>
